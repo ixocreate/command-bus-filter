fa3d529900765da261e441fc1d9f3aedfbde26f3
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-09-15 07:14:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+**This is a draft. Don't use in production**
+
+# kiwi-suite/command-bus-filter
+
+[![Build Status](https://travis-ci.org/kiwi-suite/command-bus-filter.svg?branch=master)](https://travis-ci.org/kiwi-suite/command-bus-filter)
+[![Coverage Status](https://coveralls.io/repos/github/kiwi-suite/command-bus-filter/badge.svg?branch=develop)](https://coveralls.io/github/kiwi-suite/command-bus-filter?branch=develop)
+[![Packagist](https://img.shields.io/packagist/v/kiwi-suite/command-bus-filter.svg)](https://packagist.org/packages/kiwi-suite/command-bus-filter)
+[![Packagist Pre Release](https://img.shields.io/packagist/vpre/kiwi-suite/command-bus-filter.svg)](https://packagist.org/packages/kiwi-suite/command-bus-filter)
+[![Packagist](https://img.shields.io/packagist/l/kiwi-suite/command-bus-filter.svg)](https://packagist.org/packages/kiwi-suite/command-bus-filter)
+
+## Installation
+
+The suggested installation method is via [composer](https://getcomposer.org/):
+
+```sh
+php composer.phar require kiwi-suite/command-bus-filter
+```
+
+## License
+
+kiwi-suite/command-bus is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT) - see the `LICENSE` file for details
---
+++ src/FilterHandler.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * kiwi-suite/command-bus-validation (https://github.com/kiwi-suite/command-bus-validation)
+ *
+ * @package kiwi-suite/command-bus-validation
+ * @link https://github.com/kiwi-suite/command-bus-validation
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\CommandBusValidation;
+
+use KiwiSuite\Contract\CommandBus\CommandInterface;
+use KiwiSuite\Contract\CommandBus\DispatchInterface;
+use KiwiSuite\Contract\CommandBus\HandlerInterface;
+use KiwiSuite\Contract\CommandBus\ResultInterface;
+use KiwiSuite\Filter\Filter;
+
+final class FilterHandler implements HandlerInterface
+{
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * FilterHandler constructor.
+     * @param Filter $filter
+     */
+    public function __construct(Filter $filter)
+    {
+        $this->filter = $filter;
+    }
+
+    /**
+     * @param CommandInterface $command
+     * @param DispatchInterface $dispatcher
+     * @throws \Exception
+     * @return ResultInterface
+     */
+    public function handle(CommandInterface $command, DispatchInterface $dispatcher): ResultInterface
+    {
+        if (!$this->filter->supports($command)) {
+            return $dispatcher->dispatch($command);
+        }
+
+        return $dispatcher->dispatch($this->filter->filter($command));
+    }
+}
